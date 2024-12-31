7af64d1915a73708311a842ef5c1fe9545d91bc6
Robert James <xpindia@gmail.com>
2024-12-31 17:17:51 +0530

    style-table.css worked

--- matches.php
+++ matches.php
@@ -40,7 +40,7 @@ if (!$result) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Matches</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style-table.css">
 </head>
 
 <body>
