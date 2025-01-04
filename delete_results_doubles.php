ff1db8607c30bdf0aba93c8167aa0009267d32ce
Robert James <xpindia@gmail.com>
2025-01-04 09:50:53 +0530

    results_match_doubles with edit and delete code renamed to edit_results_doubles.php and delete_results_doubles.php both the diles have the same code.

--- delete_results_doubles.php
+++ delete_results_doubles.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 
 <?php
-// results_doubles.php
+// edit_results_doubles.php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
