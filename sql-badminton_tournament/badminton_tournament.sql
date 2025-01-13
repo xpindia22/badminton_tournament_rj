8ed2e002df1632fc423ea63d51e9c34a3f081d67
Robert James <xpindia@gmail.com>
2025-01-13 23:27:32 +0530

    BiddingSql

--- sql-badminton_tournament/badminton_tournament.sql
+++ sql-badminton_tournament/badminton_tournament.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 13, 2025 at 07:30 AM
+-- Generation Time: Jan 13, 2025 at 05:56 PM
 -- Server version: 11.4.3-MariaDB-1
 -- PHP Version: 8.2.24
 
@@ -21,6 +21,15 @@ SET time_zone = "+00:00";
 -- Database: `badminton_tournament`
 --
 
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `hard_delete_match` (IN `admin_id` INT, IN `match_id` INT, IN `ip_address` VARCHAR(45))   BEGIN
+    DECLARE admin_role ENUM('admin', 'user', 'visitor')$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -36,6 +45,40 @@ CREATE TABLE `audit_logs` (
   `new_value` varchar(255) DEFAULT NULL,
   `timestamp` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bids`
+--
+
+CREATE TABLE `bids` (
+  `id` int(11) NOT NULL,
+  `player_id` int(11) NOT NULL,
+  `team_id` int(11) NOT NULL,
+  `bid_amount` decimal(10,2) NOT NULL,
+  `timestamp` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bids`
+--
+
+INSERT INTO `bids` (`id`, `player_id`, `team_id`, `bid_amount`, `timestamp`) VALUES
+(1, 12, 1, 500.00, '2025-01-13 22:30:06'),
+(2, 1, 1, 600.00, '2025-01-13 22:37:27'),
+(3, 2, 4, 100.00, '2025-01-13 22:50:17'),
+(4, 2, 4, 500.00, '2025-01-13 22:50:29'),
+(5, 2, 4, 700.00, '2025-01-13 22:50:35'),
+(6, 3, 4, 500.00, '2025-01-13 22:50:44'),
+(7, 4, 4, 500.00, '2025-01-13 22:50:53'),
+(8, 4, 4, 600.00, '2025-01-13 22:51:05'),
+(9, 6, 4, 500.00, '2025-01-13 22:51:20'),
+(10, 1, 7, 700.00, '2025-01-13 23:16:16'),
+(11, 9, 7, 700.00, '2025-01-13 23:16:27'),
+(12, 12, 7, 900.00, '2025-01-13 23:16:47'),
+(13, 10, 7, 2000.00, '2025-01-13 23:17:00'),
+(14, 11, 7, 2000.00, '2025-01-13 23:17:11');
 
 -- --------------------------------------------------------
 
@@ -165,8 +208,8 @@ INSERT INTO `matches` (`id`, `tournament_id`, `category_id`, `pool`, `player1_id
 (19, 1, 11, NULL, 3, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', '2025-01-04', '08:25', '2025-01-04', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL),
 (20, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Preliminary', '2025-01-03', '09:47', NULL, 6, 10, 12, 13, 21, 2, 2, 21, 21, 2, NULL, NULL, NULL),
 (21, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Preliminary', '2024-12-30', '12:58', NULL, 6, 2, 13, 12, 21, 4, 4, 21, 21, 2, NULL, NULL, NULL),
-(22, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Semifinals', NULL, '2025-01-04 08:37:00', NULL, 6, 2, 13, 12, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL),
-(23, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Quarterfinals', NULL, '2025-01-04 08:42:00', NULL, 10, 13, 12, 2, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL),
+(22, 1, 14, NULL, 1, 1, 0, 0, 0, 0, 21, 2, 2, 21, 0, 21, NULL, 'Pre Quarter Finals', '2025-01-13', '2025-01-04 08:37:00', NULL, 6, 2, 13, 12, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL),
+(23, 1, 14, NULL, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', '2025-01-13', '09 PM', NULL, 10, 13, 12, 2, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL),
 (24, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', NULL, '8', '2025-01-04', 2, 3, 11, 12, 21, 2, 2, 21, 21, 3, NULL, NULL, NULL),
 (25, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', NULL, '10', '2025-01-04', 2, 13, 12, 6, 21, 2, 2, 21, 21, 2, NULL, NULL, NULL),
 (26, 1, 26, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', '2025-01-06', '9', NULL, 2, 11, 13, 12, 24, 22, 22, 24, 21, 1, NULL, NULL, NULL),
@@ -181,7 +224,8 @@ INSERT INTO `matches` (`id`, `tournament_id`, `category_id`, `pool`, `player1_id
 (35, 1, 13, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Pre Quarter Finals', '2025-01-07', '17', NULL, 19, 17, 18, 13, 21, 3, 3, 21, 21, 2, NULL, NULL, NULL),
 (36, 1, 15, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Finals', '2025-01-07', '17', NULL, 4, 9, 14, 19, 26, 24, 22, 24, 21, 2, NULL, NULL, NULL),
 (37, 1, 15, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Finals', '2025-01-07', '17', NULL, 16, 9, 14, 1, 21, 3, 2, 21, 2, 21, NULL, NULL, NULL),
-(38, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Quarter Finals', '2025-01-11', '9', NULL, 21, 12, 6, 11, 21, 2, 2, 21, 21, 2, NULL, NULL, NULL);
+(38, 1, 14, NULL, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'Quarter Finals', '2025-01-11', '9', NULL, 21, 12, 6, 11, 21, 2, 2, 21, 21, 2, NULL, NULL, NULL),
+(39, 2, 1, NULL, 2, 3, 0, 0, 0, 0, 21, 2, 2, 21, 21, 2, NULL, 'Pre Quarter Finals', '2025-01-13', '20:31', '2025-01-13', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -207,36 +251,42 @@ CREATE TABLE `players` (
   `name` varchar(255) NOT NULL,
   `dob` date NOT NULL,
   `age` int(11) NOT NULL,
+  `base_price` decimal(10,2) NOT NULL DEFAULT 0.00,
   `sex` enum('M','F') NOT NULL,
   `uid` varchar(100) NOT NULL,
   `created_by` int(11) DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `category_id` int(11) DEFAULT 1
+  `category_id` int(11) DEFAULT 1,
+  `auction_status` enum('not_started','ongoing','completed') DEFAULT 'not_started',
+  `bid_start_time` datetime DEFAULT NULL,
+  `bid_end_time` datetime DEFAULT NULL,
+  `bidding_status` enum('active','closed') DEFAULT 'closed',
+  `max_bid_price` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `players`
 --
 
-INSERT INTO `players` (`id`, `name`, `dob`, `age`, `sex`, `uid`, `created_by`, `updated_at`, `category_id`) VALUES
-(1, 'Sreesha', '2008-01-01', 16, 'F', '3', 1, '2025-01-10 04:18:58', 0),
-(2, 'Eric James', '2009-05-02', 15, 'M', '1', 1, '2025-01-10 04:18:58', 0),
-(3, 'Akshaj Tiwari', '2012-01-01', 12, 'M', '2', 1, '2025-01-10 04:18:58', 0),
-(4, 'Lakshmita', '2011-01-01', 13, 'F', '4', 1, '2025-01-10 04:18:58', 0),
-(6, 'Lee Chong Wei', '1980-01-03', 44, 'M', '5', 1, '2025-01-10 04:18:58', 0),
-(9, 'Lakshaya', '2010-01-01', 15, 'F', '10', 4, '2025-01-10 04:18:58', 1),
-(10, 'Gokulan', '1990-01-01', 35, 'M', '9', 4, '2025-01-10 04:18:58', 1),
-(11, 'Zanpear', '1978-05-01', 46, 'M', '6', 4, '2025-01-10 04:18:58', 1),
-(12, 'Pandyraj', '1968-01-01', 57, 'M', '7', 4, '2025-01-10 04:18:58', 20),
-(13, 'Vijay', '1970-01-30', 54, 'M', '8', 4, '2025-01-10 04:18:58', 1),
-(14, 'Tai Tzu Ying', '1998-01-01', 27, 'F', '11', 4, '2025-01-10 04:18:58', 1),
-(15, 'An Se Young', '2000-01-01', 25, 'F', 'UID_677bbebb3b149', 4, '2025-01-10 04:18:58', 1),
-(16, 'Okuhara', '1998-01-01', 27, 'F', 'UID_677bc0ddf116b', 4, '2025-01-10 04:18:58', 1),
-(17, 'Anitha Anthony', '2008-01-01', 17, 'F', 'UID_677bc10641ef2', 4, '2025-01-10 04:18:58', 1),
-(18, 'Carolina', '1995-06-06', 29, 'F', 'UID_677bc127daf31', 4, '2025-01-10 04:18:58', 1),
-(19, 'PV Sindhu', '1995-06-07', 29, 'F', '12', 4, '2025-01-10 04:18:58', 1),
-(20, 'Victor Axelsen', '1995-05-07', 29, 'M', 'UID_677bc1915e3b3', 4, '2025-01-10 04:18:58', 1),
-(21, 'Lin Dan', '1986-02-06', 38, 'M', 'UID_677bc1a51f3b6', 4, '2025-01-10 04:18:58', 1);
+INSERT INTO `players` (`id`, `name`, `dob`, `age`, `base_price`, `sex`, `uid`, `created_by`, `updated_at`, `category_id`, `auction_status`, `bid_start_time`, `bid_end_time`, `bidding_status`, `max_bid_price`) VALUES
+(1, 'Sreesha', '2008-01-01', 16, 2000.00, 'F', '3', 1, '2025-01-13 17:55:30', 0, 'not_started', '2025-01-13 23:05:00', '2025-01-13 23:09:00', 'active', 10000.00),
+(2, 'Eric James', '2009-05-02', 15, 2000.00, 'M', '1', 1, '2025-01-13 17:55:30', 0, 'not_started', NULL, NULL, 'closed', 10000.00),
+(3, 'Akshaj Tiwari', '2012-01-01', 12, 2000.00, 'M', '2', 1, '2025-01-13 17:55:30', 0, 'not_started', NULL, NULL, 'closed', 10000.00),
+(4, 'Lakshmita', '2011-01-01', 13, 2000.00, 'F', '4', 1, '2025-01-13 17:55:30', 0, 'not_started', NULL, NULL, 'closed', 10000.00),
+(6, 'Lee Chong Wei', '1980-01-03', 44, 2000.00, 'M', '5', 1, '2025-01-13 17:55:30', 0, 'not_started', NULL, NULL, 'closed', 10000.00),
+(9, 'Lakshaya', '2010-01-01', 15, 2000.00, 'F', '10', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(10, 'Gokulan', '1990-01-01', 35, 2000.00, 'M', '9', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(11, 'Zanpear', '1978-05-01', 46, 2000.00, 'M', '6', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(12, 'Pandyraj', '1968-01-01', 57, 2000.00, 'M', '7', 4, '2025-01-13 17:55:30', 20, 'not_started', NULL, NULL, 'closed', 10000.00),
+(13, 'Vijay', '1970-01-30', 54, 2000.00, 'M', '8', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(14, 'Tai Tzu Ying', '1998-01-01', 27, 2000.00, 'F', '11', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(15, 'An Se Young', '2000-01-01', 25, 2000.00, 'F', 'UID_677bbebb3b149', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(16, 'Okuhara', '1998-01-01', 27, 2000.00, 'F', 'UID_677bc0ddf116b', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(17, 'Anitha Anthony', '2008-01-01', 17, 2000.00, 'F', 'UID_677bc10641ef2', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(18, 'Carolina', '1995-06-06', 29, 2000.00, 'F', 'UID_677bc127daf31', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(19, 'PV Sindhu', '1995-06-07', 29, 2000.00, 'F', '12', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(20, 'Victor Axelsen', '1995-05-07', 29, 2000.00, 'M', 'UID_677bc1915e3b3', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00),
+(21, 'Lin Dan', '1986-02-06', 38, 2000.00, 'M', 'UID_677bc1a51f3b6', 4, '2025-01-13 17:55:30', 1, 'not_started', NULL, NULL, 'closed', 10000.00);
 
 -- --------------------------------------------------------
 
@@ -273,6 +323,21 @@ INSERT INTO `player_access` (`id`, `player_id`, `user_id`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `player_auctions`
+--
+
+CREATE TABLE `player_auctions` (
+  `id` int(11) NOT NULL,
+  `player_id` int(11) NOT NULL,
+  `min_bid` decimal(10,2) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `status` enum('open','closed') DEFAULT 'open'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tournaments`
 --
 
@@ -296,7 +361,8 @@ INSERT INTO `tournaments` (`id`, `name`, `created_by`, `year`, `moderated_by`) V
 (7, 'xxxxsx', 1, 2025, NULL),
 (13, 'uuuuh', 5, 2025, NULL),
 (14, 'xxxxaa', 4, 2025, NULL),
-(15, 'xxdds', 4, 2025, NULL);
+(15, 'xxdds', 4, 2025, NULL),
+(17, 'xxxxxxxxx', 4, 2025, NULL);
 
 -- --------------------------------------------------------
 
@@ -338,7 +404,13 @@ INSERT INTO `tournament_categories` (`id`, `tournament_id`, `category_id`) VALUE
 (94, 7, 1),
 (97, 13, 18),
 (98, 14, 11),
-(99, 15, 11);
+(99, 15, 11),
+(100, 17, 1),
+(101, 2, 11),
+(102, 2, 11),
+(103, 2, 11),
+(104, 2, 16),
+(105, 2, 16);
 
 -- --------------------------------------------------------
 
@@ -359,6 +431,10 @@ CREATE TABLE `tournament_moderators` (
 INSERT INTO `tournament_moderators` (`id`, `tournament_id`, `user_id`) VALUES
 (2, 1, 4),
 (1, 1, 6),
+(10, 2, 4),
+(12, 2, 5),
+(11, 2, 6),
+(13, 2, 7),
 (5, 6, 4),
 (4, 13, 5),
 (3, 13, 6);
@@ -376,6 +452,7 @@ CREATE TABLE `users` (
   `mobile_no` varchar(20) DEFAULT NULL,
   `notes` text DEFAULT NULL,
   `role` enum('admin','user','visitor','moderator') NOT NULL DEFAULT 'visitor',
+  `is_team` tinyint(1) DEFAULT 0,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `email` varchar(255) DEFAULT NULL,
   `last_login` datetime DEFAULT NULL
@@ -385,13 +462,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `mobile_no`, `notes`, `role`, `created_at`, `email`, `last_login`) VALUES
-(1, 'user', '$2y$10$q0I/ctXI5pI0oUUcaTsqV.mTNeYf8evE0xKimNAvhSEooL7CIFjAW', '11111111', 'First user', 'user', '2024-12-29 03:50:28', 'user@user.com', NULL),
-(2, 'admin', '$2y$10$Vzemd6vNZoJ7tsir9lxqKuBfkPhks/ZL3mB6YRRNKRLg3H8THFdba', '7432001215', 'Admin account', 'admin', '2024-12-29 04:11:42', 'admin@admin.com', NULL),
-(4, 'xxx', '$2y$10$gv7QhzSUciynNAlwFyLSaOgPqgw9IE8jIHZn5qWhDK03QXYWNV6bm', '333', 'Hello...', 'admin', '2024-12-29 12:10:35', 'xxx@xxxx.com', NULL),
-(5, 'user2', '$2y$10$h2N1Jb3tCQ72X.KWuQaB8eUfBfJa61DULmbLDzMArIlUdtpj4im.m', '2222222222', NULL, 'user', '2024-12-31 15:28:19', 'user2@jdjdj.com', NULL),
-(6, 'user1', '$2y$10$630Wk4DbeWyToUcclXn66.2YMBCpUb8/ZwAvZwsbMU72PF3nNWdB2', '2222222222', NULL, 'user', '2025-01-10 05:55:38', 'asda@sd.asda', NULL),
-(7, 'zzz', '$2y$10$jm1Bobqhw9.TxO8.u6bC8urfHloJHZJH9bqhyRfuaSZ7HixBj.xHS', '1111111111', NULL, 'user', '2025-01-11 03:53:39', 'xxxaa@sdad.dsa', NULL);
+INSERT INTO `users` (`id`, `username`, `password`, `mobile_no`, `notes`, `role`, `is_team`, `created_at`, `email`, `last_login`) VALUES
+(1, 'user', '$2y$10$q0I/ctXI5pI0oUUcaTsqV.mTNeYf8evE0xKimNAvhSEooL7CIFjAW', '11111111', 'First user', 'user', 0, '2024-12-29 03:50:28', 'user@user.com', NULL),
+(2, 'admin', '$2y$10$Vzemd6vNZoJ7tsir9lxqKuBfkPhks/ZL3mB6YRRNKRLg3H8THFdba', '7432001215', 'Admin account', 'admin', 0, '2024-12-29 04:11:42', 'admin@admin.com', NULL),
+(4, 'xxx', '$2y$10$gv7QhzSUciynNAlwFyLSaOgPqgw9IE8jIHZn5qWhDK03QXYWNV6bm', '333', 'Hello...', 'admin', 0, '2024-12-29 12:10:35', 'xxx@xxxx.com', NULL),
+(5, 'user2', '$2y$10$h2N1Jb3tCQ72X.KWuQaB8eUfBfJa61DULmbLDzMArIlUdtpj4im.m', '2222222222', NULL, 'user', 0, '2024-12-31 15:28:19', 'user2@jdjdj.com', NULL),
+(6, 'user1', '$2y$10$630Wk4DbeWyToUcclXn66.2YMBCpUb8/ZwAvZwsbMU72PF3nNWdB2', '2222222222', NULL, 'user', 0, '2025-01-10 05:55:38', 'asda@sd.asda', NULL),
+(7, 'zzz', '$2y$10$jm1Bobqhw9.TxO8.u6bC8urfHloJHZJH9bqhyRfuaSZ7HixBj.xHS', '1111111111', NULL, 'user', 0, '2025-01-11 03:53:39', 'xxxaa@sdad.dsa', NULL);
 
 -- --------------------------------------------------------
 
@@ -419,6 +496,14 @@ ALTER TABLE `audit_logs`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `match_id` (`match_id`);
+
+--
+-- Indexes for table `bids`
+--
+ALTER TABLE `bids`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `player_id` (`player_id`),
+  ADD KEY `team_id` (`team_id`);
 
 --
 -- Indexes for table `categories`
@@ -469,6 +554,13 @@ ALTER TABLE `player_access`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `player_auctions`
+--
+ALTER TABLE `player_auctions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `player_id` (`player_id`);
+
+--
 -- Indexes for table `tournaments`
 --
 ALTER TABLE `tournaments`
@@ -517,6 +609,12 @@ ALTER TABLE `audit_logs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `bids`
+--
+ALTER TABLE `bids`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
@@ -532,7 +630,7 @@ ALTER TABLE `category_access`
 -- AUTO_INCREMENT for table `matches`
 --
 ALTER TABLE `matches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `match_details`
@@ -553,22 +651,28 @@ ALTER TABLE `player_access`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
+-- AUTO_INCREMENT for table `player_auctions`
+--
+ALTER TABLE `player_auctions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tournaments`
 --
 ALTER TABLE `tournaments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `tournament_categories`
 --
 ALTER TABLE `tournament_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
 
 --
 -- AUTO_INCREMENT for table `tournament_moderators`
 --
 ALTER TABLE `tournament_moderators`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -592,6 +696,13 @@ ALTER TABLE `user_notes`
 ALTER TABLE `audit_logs`
   ADD CONSTRAINT `audit_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `audit_logs_ibfk_2` FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`);
+
+--
+-- Constraints for table `bids`
+--
+ALTER TABLE `bids`
+  ADD CONSTRAINT `bids_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `bids_ibfk_2` FOREIGN KEY (`team_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `category_access`
@@ -623,6 +734,12 @@ ALTER TABLE `match_details`
 ALTER TABLE `player_access`
   ADD CONSTRAINT `player_access_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `player_access_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `player_auctions`
+--
+ALTER TABLE `player_auctions`
+  ADD CONSTRAINT `player_auctions_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `tournaments`
